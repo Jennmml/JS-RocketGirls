a6952000e0468ea2caf5b99f61b44d8da84f1d51
Jennifer López Miranda <je.lopez@estudiantec.cr>
2025-02-10 10:54:34 -0600

    Delete trabajoEnClase05 directory

--- trabajoEnClase05/script.js
+++
@@ -1,9 +0,0 @@
-const box = document.getElementById("box");
-
-box.addEventListener("click", function() {
-    box.classList.remove("rotate");
-
-    setTimeout(() => {
-        box.classList.add("rotate"); 
-    }, 10);
-});
