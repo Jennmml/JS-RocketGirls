80ca4424df965ee3c9099808c56ce9876a4e4163
Jenny <je.lopez@estudiantec.cr>
2025-02-10 14:21:05 -0600

    Trabajo Jennifer 5

--- trabajoEnClase05/script.js
+++ trabajoEnClase05/script.js
@@ -1,9 +1,18 @@
-const box = document.getElementById("box");
+const noButton = document.getElementById("no");
+const yesButton = document.getElementById("yes");
 
-box.addEventListener("click", function() {
-    box.classList.remove("rotate");
+/*se obtiene un valor aleatorio entre 0 y el ancho de la ventana
+ del navegador, menos 100 píxeles.*/
 
-    setTimeout(() => {
-        box.classList.add("rotate"); 
-    }, 10);
+noButton.addEventListener("mouseover", function() {
+    const x = Math.random() * (window.innerWidth - 100);
+    const y = Math.random() * (window.innerHeight - 50);
+
+    /*uso transform: translate(x, y) para mover el botón a la nueva posición generada,
+    cambia la posición del botón sin afectar su flujo del DOM.*/
+    noButton.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesButton.addEventListener("click", function() {
+    document.body.innerHTML = `<h1>Te amo</h1>`;
 });
