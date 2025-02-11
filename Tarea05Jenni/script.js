e633be8ae570de48c1aff77a572936eb02e9605c
Jenny <je.lopez@estudiantec.cr>
2025-02-11 13:23:47 -0600

    agregué un setInterval en el Script y modifiqué el tamaño del botón si en el css

--- Tarea05Jenni/script.js
+++ Tarea05Jenni/script.js
@@ -1,6 +1,14 @@
 const noButton = document.getElementById("no");
 const yesButton = document.getElementById("yes");
 
+function changeBackgroundColor() {
+    const colors = ["#ffccd5", "#f7c5cc", "#ff9aa2", "#d90429", "#ff4d6d"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+}
+
+setInterval(changeBackgroundColor, 1000);
+
 noButton.addEventListener("mouseover", function() {
     const x = Math.random() * (window.innerWidth - 100);
     const y = Math.random() * (window.innerHeight - 50);
